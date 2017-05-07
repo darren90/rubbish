e40ae13ed28ce70978b67d8ccad3c76168d9b83f
darren90 <10050522145@qq.com>
2017-05-07 10:27:01 +0800

    remove rubbish

--- iHonour/Pods/Target Support Files/Pods-iHonour/Pods-iHonour-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iHonour : NSObject
-@end
-@implementation PodsDummy_Pods_iHonour
-@end
--- iTVLive/Pods/Target Support Files/Pods-iTVLive/Pods-iTVLive-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iTVLive : NSObject
-@end
-@implementation PodsDummy_Pods_iTVLive
-@end
